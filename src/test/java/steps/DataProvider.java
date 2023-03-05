a04b12bd36723dfbc2447798895b5e898815906c
Uttam Kumer Biswas <uttamcsedu@gmail.com>
2023-03-05 11:44:00 +0600

    Update DataProvider.java

--- src/test/java/steps/DataProvider.java
+++ src/test/java/steps/DataProvider.java
@@ -2,7 +2,7 @@ package steps;
 
 public class DataProvider {
 
-	public static final String url = "https://az1d3pvglstudydesignerappqaweb.azurewebsites.net/";
+	public static final String url = "";
 	public static final String sikulipath =  System.getProperty("user.dir")+"\\src\\test\\resources\\sikuli";
 	
 	
